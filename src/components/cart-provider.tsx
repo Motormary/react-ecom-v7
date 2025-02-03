4206d06150c12cb391949568b3dc26af62ce5221
Motormary <mat.moen@gmail.com>
2025-02-03 10:45:44 +0100

    feat: animated checkout-cta + localStorage state

--- src/components/cart-provider.tsx
+++ src/components/cart-provider.tsx
@@ -1,49 +1,62 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState, ReactNode } from "react"
 
 type CartContextType = {
-  cart: string[];
-  addItem: (item: string) => void;
-  removeItem: (item: string) => void;
-  emptyCart: () => void;
-  isItemInCart: (item: string) => boolean;
-};
+  cart: string[]
+  addItem: (item: string) => void
+  removeItem: (item: string) => void
+  emptyCart: () => void
+  isItemInCart: (item: string) => boolean
+}
 
-const CartContext = createContext<CartContextType | undefined>(undefined);
+const CartContext = createContext<CartContextType | undefined>(undefined)
 
 export function CartProvider({ children }: { children: ReactNode }) {
-  const [cart, setCart] = useState<string[]>([]);
+  const localCartKey = "__ECOM_CART"
+  const localCart = localStorage.getItem(localCartKey) ?? []
+  const parsedLocalCart =
+    typeof localCart === "string" ? JSON.parse(localCart) : []
+  const [cart, setCart] = useState<string[]>(parsedLocalCart)
 
   function addItem(item: string) {
-    setCart((prev) => [...prev, item]);
+    const updatedCart = [...cart, item]
+    setCart(updatedCart)
+    localStorage.setItem(localCartKey, JSON.stringify(updatedCart))
   }
 
   function emptyCart() {
-    setCart([]);
+    setCart([])
+    localStorage.removeItem(localCartKey)
   }
 
   function removeItem(item: string) {
-    setCart((prev) =>
-      prev.filter((x) => x.toLowerCase() !== item.toLowerCase())
-    );
+    const updatedCart = cart.filter(
+      (x) => x.toLowerCase() !== item.toLowerCase()
+    )
+
+    setCart(updatedCart)
+    if (!updatedCart?.length) {
+      localStorage.removeItem(localCartKey)
+    } else {
+      localStorage.setItem(localCartKey, JSON.stringify(updatedCart))
+    }
   }
 
   function isItemInCart(item: string) {
-    return cart.some((x) => x.toLowerCase() === item.toLowerCase());
+    return cart.some((x) => x.toLowerCase() === item.toLowerCase())
   }
 
   return (
     <CartContext.Provider
-      value={{ cart, addItem, removeItem, emptyCart, isItemInCart }}
-    >
+      value={{ cart, addItem, removeItem, emptyCart, isItemInCart }}>
       {children}
     </CartContext.Provider>
-  );
+  )
 }
 
 export function useCart() {
-  const context = useContext(CartContext);
+  const context = useContext(CartContext)
   if (!context) {
-    throw new Error("useCart must be used within a CartProvider");
+    throw new Error("useCart must be used within a CartProvider")
   }
-  return context;
+  return context
 }
--- src/components/cart/cart-cta.tsx
+++ src/components/cart/cart-cta.tsx
@@ -2,17 +2,16 @@ import { ChevronRight } from "lucide-react"
 import { Link, useLocation } from "react-router"
 import { useCart } from "../cart-provider"
 import { cn } from "@/lib/utils"
+import { useMediaQuery } from "@/lib/hooks/use-media-query"
 
 const disabledPaths = ["/cart", "/checkout", "/checkout-success"]
-
-const magicBorder =
-  "before:content-[''] before:w-[104%] before:h-[102%] before:rounded-lg before:absolute before:top-[-1%] before:left-[-2%] before:-z-10 before:bg-gradient-to-r before:from-[#5ddcff] before:via-[#3c67e3] before:to-[#4e00c2] before:animate-[spin_2.5s_linear_infinite]"
 
 export default function ShoppingCartCTA() {
   const { pathname } = useLocation()
   const { cart } = useCart()
+  const isDesktop = useMediaQuery("(min-width: 768px)")
 
-  if (disabledPaths.some((path) => path === pathname.toLowerCase())) return null
+  if (disabledPaths.some((path) => path === pathname.toLowerCase()) || !isDesktop) return null
 
   return (
     <div
@@ -23,12 +22,10 @@ export default function ShoppingCartCTA() {
       <Link
         to={"/checkout"}
         className={cn(
-          magicBorder,
-          "flex relative items-center gap-1 bg-primary rounded-lg px-3 py-2 text-white hover:cursor-pointer size-fit"
+          "flex relative group items-center gap-1 bg-primary hover:bg-primary/90 rounded-lg px-3 py-2 text-white hover:cursor-pointer size-fit"
         )}>
         Checkout
-        <ChevronRight />
-        {/* <div className="absolute top-0 right-0 animate-spin -z-10 bg-conic/decreasing from-violet-700 via-lime-300 to-violet-700 w-full h-full"></div> */}
+        <ChevronRight className="animate-wiggle group-hover:animate-none" />
       </Link>
     </div>
   )
